8dc8bec02f9a1f6a25135e94cbc01956b6ad276f
redeagle17 <ankursingh1438@gmail.com>
2026-01-14 22:27:05 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
