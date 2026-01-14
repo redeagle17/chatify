eda7118dfc0d92acac0ec6cb2f1702d92bfa6fbf
redeagle17 <ankursingh1438@gmail.com>
2026-01-15 02:10:46 +0530

    Signup and welcome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,24 @@
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
-dotenv.config();
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 4000;
+const PORT = ENV.PORT || 4000;
 
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json({ limit: "5mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
+app.use("/api/auth", authRoutes);
+
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
@@ -23,4 +28,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
