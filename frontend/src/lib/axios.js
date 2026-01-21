4948d7c59d93da3147b55c11e1082700ce8e91c2
redeagle17 <ankursingh1438@gmail.com>
2026-01-21 23:09:32 +0530

    setting up frontend for aws deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "http://13.203.221.235:4000/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "http://43.204.196.183:4000/api",
   withCredentials: true,
 });
